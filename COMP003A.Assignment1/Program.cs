842371886793ed01289719dae5ee58d029f0365a
lpj11 <lpj11@DESKTOP-1SD19HD>
2024-01-18 00:13:56 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,115 @@
+﻿/* 
+ * Author:      Logan Jones
+ * Course:      COMP-003A-L01
+ * Purpose:     Code for Assingment 1
+ */
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1. Hello World");
+            Console.WriteLine("2. I am Logan");
+            Console.WriteLine("3. I am male");
+            Console.WriteLine("4. I am 5 foot 10 inches");
+            Console.WriteLine("5. My hair is brown");
+            Console.WriteLine("6. I am white ");
+            Console.WriteLine("7. Got hands");
+            Console.WriteLine("8. I built my computer");
+            Console.WriteLine("9. I collect gundam");
+            Console.WriteLine("10. I got bof eyes");
+            Console.WriteLine("11. I am a star wars fan");
+            Console.WriteLine("12. I habitually bite my fingernails");
+            Console.WriteLine("13. I have glasses");
+            Console.WriteLine("14. I spend too much on collectibles");
+            Console.WriteLine("15. I have no taste in shoes");
+            Console.WriteLine("16. I have dropped out of college 3 times");
+            Console.WriteLine("17. I have size 13 shoes");
+            Console.WriteLine("18. I work at Savemart");
+            Console.WriteLine("19. I bought Spiderman 2 before a ps5");
+            Console.WriteLine("20. I studied engineering in high school");
+            Console.WriteLine("21. I've gotten third degree sunburn");
+            Console.WriteLine("22. I tried learning blender");
+            Console.WriteLine("23. I do not drink coffee");
+            Console.WriteLine("24. I like building model kits");
+            Console.WriteLine("25. I like photography");
+            Console.WriteLine("26. I have so many cats");
+            Console.WriteLine("27. I drive a truck");
+            Console.WriteLine("28. Said truck needs to be fixed");
+            Console.WriteLine("29. I am so single");
+            Console.WriteLine("30. I had leg surgery in 2021");
+            Console.WriteLine("31. This is so many things");
+            Console.WriteLine("32. I do not clean my glasses enough");
+            Console.WriteLine("33. I am a washed gamer");
+            Console.WriteLine("34. I hope I am still good at coding");
+            Console.WriteLine("35. 64 facts remainging on the wall");
+            Console.WriteLine("36. Gurren Lagann is a great anime");
+            Console.WriteLine("37. Godzilla Minus One was a masterpiece");
+            Console.WriteLine("38. I love movies");
+            Console.WriteLine("39. Disecting movies is very fun");
+            Console.WriteLine("40. I want my own apartment so bad");
+            Console.WriteLine("41. I have never read a comic book");
+            Console.WriteLine("42. Pirate manga or bust");
+            Console.WriteLine("43. Why are video games so bad these days");
+            Console.WriteLine("44. I miss when a selling point was the quality");
+            Console.WriteLine("45. I want to make stuff");
+            Console.WriteLine("46. Why making video games take so long");
+            Console.WriteLine("47. I want better male jewelry");
+            Console.WriteLine("48. I once coded a bad elevator");
+            Console.WriteLine("49. I like turtles");
+            Console.WriteLine("50. Woahhhh, we are halfway there");
+            Console.WriteLine("51. I usually only shop for sales");
+            Console.WriteLine("52. I want to play games with friends");
+            Console.WriteLine("53. why are board games so expensive");
+            Console.WriteLine("54. I live at home");
+            Console.WriteLine("55. I use both shampoo AND conditioner");
+            Console.WriteLine("56. I should drink less soda");
+            Console.WriteLine("57. I am loving the new Percy Jackson show");
+            Console.WriteLine("58. I want to write a fantasy novel");
+            Console.WriteLine("59. Lethal Company is so good");
+            Console.WriteLine("60. What is a good management certificate");
+            Console.WriteLine("61. I do not have enough facts for this");
+            Console.WriteLine("62. I think speedrunners are insane");
+            Console.WriteLine("63. Hades is a wonderful game");
+            Console.WriteLine("64. I am not good with horror");
+            Console.WriteLine("65. If bears are not friends, why so fluffy");
+            Console.WriteLine("66. What year did the War of 1812 take place");
+            Console.WriteLine("67. I enjoy learning random facts");
+            Console.WriteLine("68. I like clicky clacky dice");
+            Console.WriteLine("69. I have written 69 facts, nice");
+            Console.WriteLine("70. Office chairs suck");
+            Console.WriteLine("71. Where do I get a comfortable chair");
+            Console.WriteLine("72. My headphones are Seinheiser");
+            Console.WriteLine("73. My everything else is Amazon basics");
+            Console.WriteLine("74. I have a torch shaped like a lightsaber");
+            Console.WriteLine("75. I don't know how to use a grill");
+            Console.WriteLine("76. I don't wear half of my wardrobe");
+            Console.WriteLine("77. My most used outfit is my work uniform");
+            Console.WriteLine("78. Do you have an internal monologue");
+            Console.WriteLine("79. How much of a monologue can you have before it becomes schizophrenia");
+            Console.WriteLine("80. These last few will be like pulling teeth");
+            Console.WriteLine("81. I am right-handed");
+            Console.WriteLine("82. I have not done calculus in 2 years");
+            Console.WriteLine("83. I could sooner tell you economic principles than I could solve sin(30)");
+            Console.WriteLine("84. Are you still reading these?");
+            Console.WriteLine("85. 84 is the only question properly ended with a question mark");
+            Console.WriteLine("86. I don't think most things should take more than a week to learn");
+            Console.WriteLine("87. They end up taking so much longer");
+            Console.WriteLine("88. I spend so much money on fun, I would have so much saved up by now");
+            Console.WriteLine("89. Living is expensive");
+            Console.WriteLine("90. I hate sales tactics");
+            Console.WriteLine("91. Sales should be good enough to make themselves");
+            Console.WriteLine("92. If you have to convince people to buy from you, you have a bad business");
+            Console.WriteLine("93. Some people in management are definitely nepo babies");
+            Console.WriteLine("94. There is a developmental difference between broke and rich people");
+            Console.WriteLine("95. Rich people cannot develope common sense and judgment since they never struggle through problem solving");
+            Console.WriteLine("96. A rich poor man fixes issues");
+            Console.WriteLine("97. A poor rich man cries");
+            Console.WriteLine("98. I don't think being rude should be so frowned upon");
+            Console.WriteLine("99. It is also rude to not pay people their wages, so why am I the bad guy for saying it");
+            Console.WriteLine("100. Writing 100 fcats about me took way too long");
+        }
+    }
+}
